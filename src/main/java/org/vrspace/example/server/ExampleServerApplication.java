c3fd47d38af742d016f721575d37a1812abb451f
Josip Almasi <joe@vrspace.org>
2024-04-12 12:19:30 +0100

    spring upgrade, formatting

--- src/main/java/org/vrspace/example/server/ExampleServerApplication.java
+++ src/main/java/org/vrspace/example/server/ExampleServerApplication.java
@@ -4,16 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "org.vrspace.server", "org.vrspace.example" })
-public class ExampleServerApplication extends WebMvcConfigurerAdapter {
+public class ExampleServerApplication implements WebMvcConfigurer {
 
   public static void main(String[] args) {
     SpringApplication.run(ExampleServerApplication.class, args);
   }
 
+  @Override
   public void addViewControllers(ViewControllerRegistry registry) {
     registry.addViewController("/login").setViewName("login");
     registry.addViewController("/index").setViewName("index");
