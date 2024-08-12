fe07457f183715b75b6e1be8e356b02ca096d78f
Josip Almasi <joe@vrspace.org>
2024-08-12 13:28:38 +0100

    upgrade to spring boot 3

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ A real-life scenario - I want only registered users allowed.
 # how to use
 
 - import the maven project into your IDE
+- copy this sausage to VM arguments: --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.util.concurrent=ALL-UNNAMED --add-opens java.base/sun.net.www.protocol.http=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED
 - start ExampleServerApplication as spring boot app
 - access http://localhost:8080/ with TWO browsers
 - log in as user1/pass1 in first, and user2/pass2 in second browser
--- src/main/java/org/vrspace/example/server/MySecurityConfig.java
+++ src/main/java/org/vrspace/example/server/MySecurityConfig.java
@@ -2,31 +2,34 @@ package org.vrspace.example.server;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class MySecurityConfig extends WebSecurityConfigurerAdapter {
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().authenticated();
-    http.formLogin().failureUrl("/login?error").defaultSuccessUrl("/").loginPage("/login").permitAll().and().logout()
-        .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll();
+public class MySecurityConfig {
+
+  @Bean
+  SecurityFilterChain configureSecurity(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
+    http.formLogin(form -> form.failureUrl("/login?error").defaultSuccessUrl("/").loginPage("/login").permitAll());
+    http.logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
+        .permitAll());
+    return http.build();
   }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+  @Bean
+  InMemoryUserDetailsManager configureUsers() throws Exception {
     UserDetails user1 = User.withUsername("user1").password(passwordEncoder().encode("pass1")).roles("USER").build();
     UserDetails user2 = User.withUsername("user2").password(passwordEncoder().encode("pass2")).roles("USER").build();
-    auth.inMemoryAuthentication().withUser(user1).withUser(user2);
+    return new InMemoryUserDetailsManager(user1, user2);
   }
 
   @Bean
